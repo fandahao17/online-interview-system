8b703602a5a08308006e9b47a09449ee12b768f5
xuyuming <xuyuming@mail.ustc.edu.cn>
2020-05-28 12:25:47 +0800

    add editor

--- Online_Interview_System/frontend/src/intvweeviews/Editor.vue
+++ Online_Interview_System/frontend/src/intvweeviews/Editor.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="editor-window">
-    editor
+    <editor
+      apiKey="hc02du3nr8ugtoouiyovc99dnozuzms34pkgrnckrfpr6mkr"
+      plugins="codesample"
+      :init="{ height:500 }"
+      />
   </div>
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue'
 export default {
-  name: 'EditorWindow'
+  name: 'EditorWindow',
+  components: {
+    'editor': Editor
+  }
 }
 </script>
 
