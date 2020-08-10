22c31c2cc98dfefe89378e1c172e2b7fbf7b65f0
cya <cya990524@gmail.com>
2020-08-10 14:59:04 +0800

    add hr recall interview page(not finish)

---
+++ Online_Interview_System/frontend/src/hrs/HrRecall.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <el-container>
+      <el-header><head-menu></head-menu></el-header>
+      <el-main>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import HeadMenu from '@/hrs/HeadMenu'
+
+export default {
+  name: 'HrRecall',
+  components: {
+    HeadMenu
+  },
+  data: function () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #DCDFE6;
+    text-align: center;
+    line-height: 55px;
+  }
+
+  .el-main {
+    color: #333;
+    text-align: center;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
