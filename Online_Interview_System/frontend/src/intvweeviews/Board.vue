6686dc407aa8846924ca713e42c069af861ac050
LiTwotwo <hnjylcy@mail.ustc.edu.cn>
2020-08-12 18:57:37 +0800

    create board with roomid

--- Online_Interview_System/frontend/src/intvweeviews/Board.vue
+++ Online_Interview_System/frontend/src/intvweeviews/Board.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="board-window">
-    white board
+    <iframe :src="this.privateurl" width="100%" frameborder="0" height="500px" scrolling="no"></iframe>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BoardWindow'
+  name: 'BoardWindow',
+  data: function () {
+    return {
+      privateurl: ''
+    }
+  },
+  mounted: function () {
+    this.privateurl = 'http://localhost:8001/boards/' + this.$route.params.roomid
+  }
 }
 </script>
 
