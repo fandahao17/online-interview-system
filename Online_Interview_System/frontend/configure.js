6bc5ec23bb013e6c4142a14e2cc1bf2a2c728f2d
xushuoxiang <xushuoxiang@mail.ustc.edu.cn>
2020-08-22 01:00:19 +0800

    add video.vue

--- Online_Interview_System/frontend/configure.js
+++ Online_Interview_System/frontend/configure.js
@@ -3,9 +3,7 @@ const API_PORT = '3000';
 const API_ROOT = `http://106.14.227.202:${API_PORT}`;
 
 const DEFAULT_ICE_SERVER = {
-  url: 'turn:106.14.227.202:3478',
-  credential: 'interview',
-  username: 'interview',
+  url: 'stun:stun.l.google.com:19302'
 };
 
 module.exports = { API_PORT, API_ROOT, DEFAULT_ICE_SERVER };
