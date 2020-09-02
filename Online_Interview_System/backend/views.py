8e6085f01f20804b2376720888d917b69ab75ea3
xuyuming <xuyuming@mail.ustc.edu.cn>
2020-09-02 15:14:11 +0800

    fix django

--- Online_Interview_System/backend/views.py
+++ Online_Interview_System/backend/views.py
@@ -303,17 +303,17 @@ def room_getvideolist(request, roomid):
 	return JsonResponse({'result': 'successfully','videolist':videolist})
 
 def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
-  	with open(file_name, "rb") as f:
-    	f.seek(offset, os.SEEK_SET)
-    	remaining = length
-    	while True:
-     		bytes_length = chunk_size if remaining is None else min(remaining, chunk_size)
-      		data = f.read(bytes_length)
-      		if not data:
-        		break
-      		if remaining:
-        		remaining -= len(data)
-      		yield data
+	with open(file_name, "rb") as f:
+		f.seek(offset, os.SEEK_SET)
+		remaining = length
+		while True:
+			bytes_length = chunk_size if remaining is None else min(remaining, chunk_size)
+			data = f.read(bytes_length)
+			if not data:
+				break
+			if remaining:
+				remaining -= len(data)
+			yield data
 
 @api_view(['GET'])
 def room_getvideo(request, roomid):
@@ -329,23 +329,23 @@ def room_getvideo(request, roomid):
 	dir = os.path.join(os.path.join(BASE_DIR, 'static'),str(roomid))
 	path = os.path.join(dir, request.data.filename)
 	range_header = request.META.get('HTTP_RANGE', '').strip()
-  	range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
-  	range_match = range_re.match(range_header)
-  	size = os.path.getsize(path)
-  	content_type, encoding = mimetypes.guess_type(path)
-  	content_type = content_type or 'application/octet-stream'
-  	if range_match:
-    	first_byte, last_byte = range_match.groups()
-    	first_byte = int(first_byte) if first_byte else 0
-    	last_byte = first_byte + 1024 * 1024 * 8    # 8M 每片,响应体最大体积
-    	if last_byte >= size:
-      	last_byte = size - 1
-    	length = last_byte - first_byte + 1
-    	resp = StreamingHttpResponse(file_iterator(path, offset=first_byte, length=length), status=206, content_type=content_type)
-    	resp['Content-Length'] = str(length)
-    	resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
-    resp['Accept-Ranges'] = 'bytes'
-	return 
+	range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+	range_match = range_re.match(range_header)
+	size = os.path.getsize(path)
+	content_type, encoding = mimetypes.guess_type(path)
+	content_type = content_type or 'application/octet-stream'
+	if range_match:
+		first_byte, last_byte = range_match.groups()
+		first_byte = int(first_byte) if first_byte else 0
+		last_byte = first_byte + 1024 * 1024 * 8    # 8M 每片,响应体最大体积
+		if last_byte >= size:
+			last_byte = size - 1
+		length = last_byte - first_byte + 1
+		resp = StreamingHttpResponse(file_iterator(path, offset=first_byte, length=length), status=206, content_type=content_type)
+		resp['Content-Length'] = str(length)
+		resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
+	resp['Accept-Ranges'] = 'bytes'
+	return resp
 
 @api_view(['POST'])
 def room_add(request):
