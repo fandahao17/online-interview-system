a690ba29782457dfcca7895184280f98c644eddd
xuyuming <xuyuming@mail.ustc.edu.cn>
2020-09-05 23:45:50 +0800

    nothing

--- Online_Interview_System/frontend/src/intvweeviews/Index.vue
+++ Online_Interview_System/frontend/src/intvweeviews/Index.vue
@@ -18,7 +18,7 @@
         <el-col :span="13" class="editor-and-board">
           <el-tabs type="border-card" class="card">
             <el-tab-pane label="editor" class="editor-window">
-              <editor-window isHr="false"></editor-window>
+              <editor-window :isHr="false"></editor-window>
             </el-tab-pane>
             <el-tab-pane label="white board" class="board-window">
               <board-window isHr="false"></board-window>
