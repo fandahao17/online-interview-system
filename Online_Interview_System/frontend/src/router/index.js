788ea2cf312f9b3c1fb3921cafdb1b0e7bb36969
fandahao <fandahao17@mail.ustc.edu.cn>
2020-09-05 15:04:32 +0800

    Add page titles

--- Online_Interview_System/frontend/src/router/index.js
+++ Online_Interview_System/frontend/src/router/index.js
@@ -22,67 +22,106 @@ var router = new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        title: '首页 - 在线面试平台'
+      }
     },
     {
       path: '/admin',
       name: 'Admin',
-      component: AdminHome
+      component: AdminHome,
+      meta: {
+        title: '管理员 - 在线面试平台'
+      }
     },
     {
       path: '/interviewee/:roomid',
       name: 'Interviewee',
-      component: IntvweeHome
+      component: IntvweeHome,
+      meta: {
+        title: '面试 - 在线面试平台'
+      }
     },
     {
       path: '/interviewer/:roomid',
       name: 'InterviewerRoom',
-      component: ItvrRoom
+      component: ItvrRoom,
+      meta: {
+        title: '面试 - 在线面试平台'
+      }
     },
     {
       path: '/interviewer',
       name: 'Interviewer',
-      component: IntvwerHome
+      component: IntvwerHome,
+      meta: {
+        title: '面试官 - 在线面试平台'
+      }
     },
     {
       path: '/schedule',
       name: 'ScheduleView',
-      component: ScheduleView
+      component: ScheduleView,
+      meta: {
+        title: '面试安排 - 在线面试平台'
+      }
     },
     {
       path: '/login',
       name: 'Login',
-      component: LogRegHome
+      component: LogRegHome,
+      meta: {
+        title: '登陆 - 在线面试平台'
+      }
     },
     {
       path: '/register',
       name: 'Register',
-      component: RegisterHome
+      component: RegisterHome,
+      meta: {
+        title: '注册 - 在线面试平台'
+      }
     },
     {
       path: '/hr',
       name: 'Hr',
-      component: HrHome
+      component: HrHome,
+      meta: {
+        title: 'HR - 在线面试平台'
+      }
     },
     {
       path: '/watch',
       name: 'Watch',
-      component: HrWatch
+      component: HrWatch,
+      meta: {
+        title: '观看面试 - 在线面试平台'
+      }
     },
     {
       path: '/hr/:roomid',
       name: 'HrviewRoom',
-      component: HrWatchHome
+      component: HrWatchHome,
+      meta: {
+        title: '观看面试 - 在线面试平台'
+      }
     },
     {
       path: '/recall',
       name: 'Recall',
-      component: HrRecall
+      component: HrRecall,
+      meta: {
+        title: '回溯面试 - 在线面试平台'
+      }
     },
     {
       path: '/recall/:roomid/:filename',
       name: 'RecallVideo',
-      component: HrVideo
+      component: HrVideo,
+      meta: {
+        title: '回溯面试 - 在线面试平台'
+      }
     },
     {
       path: '*',
@@ -94,6 +133,10 @@ var router = new Router({
 router.beforeEach((to, from, next) => {
   if (to.path === '/login') {
     window.hideLogin = false
+  }
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
   }
   next()
 })
